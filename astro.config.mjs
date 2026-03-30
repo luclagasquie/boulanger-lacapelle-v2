614ba3cf22b82510f80d49322fc8356c828c1e3f
Luc Lagasquie <sensodia@gmail.com>
2026-03-30 13:03:27 +0200

    Fix Cloudflare Pages build requirements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, sessionDrivers } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: "passthrough",
+    inspectorPort: false
+  }),
+  session: {
+    driver: sessionDrivers.lruCache()
+  },
   vite: {
     server: {
       host: true
